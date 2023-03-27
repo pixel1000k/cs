c530f8fe4c3f1b501172859721f90f4011ab0f74
pixel1000k <116036165+pixel1000k@users.noreply.github.com>
2023-03-27 21:36:23 +0200

    Add files via upload

---
+++ rozkladliczb/rozkladliczb/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rozkladliczb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt16(Console.ReadLine());
+            int a = input;
+            while (a != 1)
+            {   
+                for (int i = 2; i <= a; i++)
+                {
+                    if (a % i == 0)
+                    {
+                        a = a / i;
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
