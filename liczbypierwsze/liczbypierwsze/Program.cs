031260c0034f178893d300c734e6acc25c4b6c89
pixel1000k <116036165+pixel1000k@users.noreply.github.com>
2023-03-27 21:35:16 +0200

    Add files via upload

---
+++ liczbypierwsze/liczbypierwsze/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace liczbypierwsze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt16(Console.ReadLine());
+            bool LiczbaPierwsza = true;
+            for (int i = 2; i*i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    LiczbaPierwsza = false;
+                }
+            }
+            Console.WriteLine(LiczbaPierwsza);
+        }
+    }
+}
